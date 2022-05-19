6874357141b52168bd7393c551caec9887b93526
Рыбко Егор Андреевич <erybko@phoenixit.ru>
2022-05-19 19:57:27 +0300

    add program

---
+++ Calc/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, я калькулятор!");
+            int a = 0;
+            int b = 0;
+            char d;
+            Console.WriteLine("Введите первое число:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Выбирете действие: + - * /");
+            d = Convert.ToChar(Console.ReadLine());
+            if(d == '+')
+            {
+                Console.WriteLine(a + b);
+            }
+            else if(d == '-')
+            {
+                Console.WriteLine(a - b);
+            }
+            else if (d == '*')
+            {
+                Console.WriteLine(a * b);
+            }
+            else if (d == '/')
+            {
+                Console.WriteLine(a / b);
+            }
+        }
+    }
+}
