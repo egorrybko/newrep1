5f27622952ac1b513412d58f794e0f2696d4e748
Рыбко Егор Андреевич <erybko@phoenixit.ru>
2022-05-19 20:41:30 +0300

    new logic

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -6,35 +6,43 @@ namespace Calc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет, я калькулятор! Меня зовут Том." );
-            int a = 0;
-            int b = 0;
-            char d;
-            Console.WriteLine("Введите первое число:");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите второе число:");
-            b = Convert.ToInt32(Console.ReadLine());
-            if(b == 0)
+            while (true)
             {
-                Console.WriteLine("Внимание b = 0, деление невозможно!");
-            }
-            Console.WriteLine("Выбирете действие: + - * /");
-            d = Convert.ToChar(Console.ReadLine());
-            if(d == '+')
-            {
-                Console.WriteLine(a + b);
-            }
-            else if(d == '-')
-            {
-                Console.WriteLine(a - b);
-            }
-            else if (d == '*')
-            {
-                Console.WriteLine(a * b);
-            }
-            else if (d == '/')
-            {
-                Console.WriteLine(a / b);
+                Console.WriteLine("Привет, я калькулятор! Меня зовут Том.");
+                int a = 0;
+                int b = 0;
+                char d;
+                Console.WriteLine("Введите первое число:");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите второе число:");
+                b = Convert.ToInt32(Console.ReadLine());
+                if (b == 0)
+                {
+                    Console.WriteLine("Внимание b = 0, деление невозможно!");
+                }
+                Console.WriteLine("Выбирете действие: + - * /");
+                d = Convert.ToChar(Console.ReadLine());
+                if (d == '+')
+                {
+                    Console.WriteLine(a + b);
+                }
+                else if (d == '-')
+                {
+                    Console.WriteLine(a - b);
+                }
+                else if (d == '*')
+                {
+                    Console.WriteLine(a * b);
+                }
+                else if (d == '/')
+                {
+                    Console.WriteLine(a / b);
+                }
+                Console.WriteLine("Хотите ещё считать? 1 - да, 2 - нет");
+                if(Console.ReadLine() == "2")
+                {
+                    return;
+                }
             }
         }
     }
